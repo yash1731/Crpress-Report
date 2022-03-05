7517a3dad4804504bf21adba4a52033e6e376094
reyansh <yash.dewangan82@gmail.com>
2022-03-05 22:29:57 +0000

    test update-10

--- cypress/integration/ProceedToCheckout.spec.js
+++ cypress/integration/ProceedToCheckout.spec.js
@@ -1,39 +1,53 @@
 
-context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('http://automationpractice.com/index.php')
-    })
+/// <reference types="cypress" />
 
+describe('Basic Desktop Tests', () =>{
 
-describe('Proceed to checkout Test', function(){   
+  beforeEach( () => {
+      cy.fixture('example').then((user) => {
+      cy.visit(user.url)  
+    })   
+  })
 
-    it('Verify checkout page', function(){
-      cy.fixture('example').then((user)=>{
-      cy.visit(user.url)
-      cy.title().should('eq','My Store')
-      })
-    })
+  it('Test checkout page', () => {
+      //cy.viewport(1280,720)
+      //cy.viewport('ipad-2')     
 
-    it('Verify checkout page - Add product to cart', function(){
-      cy.fixture('example').then((user)=>{
-      cy.visit(user.url)
-      cy.contains('Women').click()
-      
-      cy.scrollTo('bottom')
-      cy.wait(500)
-      cy.get('.last-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
-      //cy.contains('Add to Cart').click()      
-      cy.wait(1000)
-      cy.contains('Proceed to checkout').click()
-      cy.scrollTo('bottom')
-      cy.wait(500)
+      // verify product page
+      cy.contains('Women').should('exist')
+      cy.contains('Women').should('have.text','Women')        
+      cy.get('div')
+      cy.get('#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
+
+      cy.get('.button-medium > span').click() 
       cy.get('.cart_navigation > .button > span').click()
+
+      // verify page url
+      cy.url().should('include','order')
+
+      // verify page and links
+      cy.contains('Authentication').should('exist')
+      cy.contains('Create an account').should('exist')
+      cy.contains('Sign in').should('exist')
+      cy.get('#SubmitLogin > span').click()
+            
+  })
+
+  it('Log in page test', () => {
+
+      cy.fixture('example').then((user) => {
+      cy.get('.login').click()
+
+       // verify page and links
+       
+      cy.url().should('include','my-account')       
       cy.get('#email').type(user.email)
       cy.get('#passwd').type(user.password)
-      cy.wait(500)
-      cy.contains('Sign in').click()
-     
-      })
-     })  
-   })
+      cy.get('#SubmitLogin > span').click()
+      cy.contains('Yashpal DEWANGAN').should('exist')
+
+      })      
+
+  })   
+  
 })
